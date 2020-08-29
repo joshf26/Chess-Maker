f9717c1ac2df0aa713e58a1c520e3738668c47d3
Joshua Franklin <joshuafranklin26@gmail.com>
2020-08-28 20:17:10 -0600

    Update Jousting to use new info API

--- app/chess_maker/packs/party/controllers/jousting.py
+++ app/chess_maker/packs/party/controllers/jousting.py
@@ -1,9 +1,7 @@
 from __future__ import annotations
-from __future__ import annotations
 
 import asyncio
-from typing import TYPE_CHECKING, Dict, Generator
-
+from typing import Dict, Generator
 from color import Color
 from controller import Controller
 from info_elements import InfoButton, InfoText
@@ -11,9 +9,7 @@ from option import IntOption
 from packs.standard.pieces.knight import Knight
 from piece import Direction, Piece
 from vector2 import Vector2
-
-if TYPE_CHECKING:
-    from ply import Ply
+from ply import Ply, DestroyAction
 
 
 class Jousting(Controller):
@@ -30,14 +26,13 @@ class Jousting(Controller):
         Color.PURPLE,
     ]
     options = {
-        'Game Start Timer': IntOption(3, 0)
+        'Game Start Timer': IntOption(3, 0),
     }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.game_started = False
-        self.start_button = InfoButton('Start Game', self._start_game)
 
     def init_board(self, board: Dict[Vector2, Piece]) -> None:
         board[Vector2(0, 2)] = Knight(Color.WHITE, Direction.NORTH)
@@ -49,14 +44,15 @@ class Jousting(Controller):
         board[Vector2(5, 0)] = Knight(Color.BLUE, Direction.NORTH)
         board[Vector2(2, 0)] = Knight(Color.PURPLE, Direction.NORTH)
 
-        with self.game.public_info_elements as info:
-            info.append(self.start_button)
+        self.game.update_public_info([InfoButton('Start Game', self._start_game)])
 
     def get_plies(self, color: Color, from_pos: Vector2, to_pos: Vector2) -> Generator[Ply]:
         if self.game_started:
             piece = self.game.board[from_pos]
             if color == piece.color:
                 yield from piece.get_plies(from_pos, to_pos, self.game.game_data)
+            else:
+                self.game.send_error(color, 'That is not your piece.')
         else:
             self.game.send_error(color, 'The game has not started yet.')
 
@@ -66,29 +62,20 @@ class Jousting(Controller):
 
     def _start_game(self, color: Color):
         async def countdown():
-            self._clear_unused()
-
-            # Remove the start game button and add the countdown timer element.
-            with self.game.public_info_elements as info:
-                info.remove(self.start_button)
-                info.append(InfoText(''))
+            self.game.apply_ply(None, Ply('Clear Board', [
+                DestroyAction(pos)
+                for pos in self.game.board.keys()
+                if self.game.board[pos].color not in self.game.players
+            ]))
 
             # Tick the countdown the set number of times.
             for timer in range(self.options['Game Start Timer'].value, 0, -1):
-                with self.game.public_info_elements as info:
-                    info[0].text = f'Game starting in {timer}'
-
+                self.game.update_public_info([InfoText(f'Game starting in {timer}')])
                 await asyncio.sleep(1)
 
             # Remove the countdown.
-            with self.game.public_info_elements as info:
-                del info[0]
+            self.game.update_public_info([])
 
             self.game_started = True
 
         asyncio.create_task(countdown())
-
-    def _clear_unused(self):
-        for pos, piece in list(self.game.board.items()):
-            if piece.color not in self.game.players:
-                del self.game.board[pos]
